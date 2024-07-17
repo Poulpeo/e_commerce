07ef1232cda6abc7773c4e13ab213576cbcba8de
Poulpeo <geoffroy.marsault@netcourrier.com>
2024-07-17 15:29:17 -0400

    clients can create orders and update them by adding items to the order created

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args) {
+        Order order = new Order();
+        order.attach(new PriceObserver());
+        order.attach(new QuantityObserver());
+
+        order.addItem(new Item("Item 1", 50));
+        System.out.println(order);
+
+        order.addItem(new Item("Item 2", 180));
+        System.out.println(order);
+
+        order.addItem(new Item("Item 3", 50));
+        System.out.println(order);
+
+        order.addItem(new Item("Item 4", 20));
+        order.addItem(new Item("Item 5", 10));
+        order.addItem(new Item("Item 6", 10));
+        System.out.println(order);
     }
 }
